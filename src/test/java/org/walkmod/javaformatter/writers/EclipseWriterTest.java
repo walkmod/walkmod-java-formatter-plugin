6253cf525105cf4546a642a946cadeb3af814efe
rpau <raquelpau@gmail.com>
2015-02-02 18:44:54 +0100

    java 8 support

--- src/test/java/org/walkmod/javaformatter/writers/EclipseWriterTest.java
+++ src/test/java/org/walkmod/javaformatter/writers/EclipseWriterTest.java
@@ -54,5 +54,28 @@ public class EclipseWriterTest {
 		outputDir.delete();
 		Assert.assertTrue(code.contains("\t"));
 	}
+	
+	@Test
+	public void testLambdaFormatConfig() throws Exception{
+		EclipseWriter ew = new EclipseWriter();
+		ew.setConfigFile("src/test/resources/eclipse-style.xml");
+
+		VisitorContext ctx = new VisitorContext();
+		File outputDir = new File("src/test/output");
+		outputDir.mkdirs();
+		File output = new File(outputDir, "Foo.java");
+
+		ctx.put(AbstractWalker.ORIGINAL_FILE_KEY, output);
+
+		ew.write("public class Foo {\n\tObject o = s -> String::length;\n int b = 0;}", ctx);
+
+		ew.close();
+
+		String code = FileUtil.readEntirely(output);
+
+		output.delete();
+		outputDir.delete();
+		Assert.assertTrue(code.contains("o = s-> String"));
+	}
 
 }
